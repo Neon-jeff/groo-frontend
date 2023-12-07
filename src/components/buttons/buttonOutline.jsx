3ef6ae38d8dff3ce9e1cf8607c70f1c626c63184
Neon-jeff <omaksteejay@gmail.com>
2023-12-07 11:48:52 +0100

    Steps Section

--- src/components/buttons/buttonOutline.jsx
+++ src/components/buttons/buttonOutline.jsx
@@ -8,8 +8,8 @@ export default function ButtonOutiline({ text, width, action }) {
         action();
       }}
     >
-      <div className="absolute bg-transparent border-2 border-black w-full h-full rounded-md"></div>
-      <div className="bg-white text-black text-xl  h-full border-2 border-black  translate-x-2 -translate-y-2 flex items-center justify-center  rounded-md  font-medium">
+      <div className="absolute bg-transparent border-[2px] border-black w-full h-full rounded-md"></div>
+      <div className="bg-white text-black text-xl  h-full border-[2px] border-black  translate-x-2 -translate-y-2 flex items-center justify-center  rounded-md  font-medium">
         {text}
       </div>
     </button>
--- src/components/buttons/buttonSecondary.jsx
+++ src/components/buttons/buttonSecondary.jsx
@@ -8,8 +8,8 @@ export default function ButtonSecondary({text,width,action}) {
         action();
       }}
     >
-      <div className="absolute top-0 bg-black-white-200  border-2 border-black w-full h-full rounded-xl"></div>
-      <div className="bg-saltpan-50 text-black text-xl  h-full border-2 border-black  translate-x-2 -translate-y-2 flex items-center justify-center  rounded-xl  font-medium">
+      <div className="absolute top-0 bg-black-white-200  border-[2px] border-black w-full h-full rounded-xl"></div>
+      <div className="bg-saltpan-50 text-black text-xl  h-full border-[2px] border-black  translate-x-2 -translate-y-2 flex items-center justify-center  rounded-xl  font-medium">
         {text}
       </div>
     </button>
