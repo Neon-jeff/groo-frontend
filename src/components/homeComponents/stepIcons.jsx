95e3b5a8931840bf18e311a538a5fff95bbae22c
Neon-jeff <omaksteejay@gmail.com>
2023-12-08 01:06:22 +0100

    Styles change

--- src/components/homeComponents/stepIcons.jsx
+++ src/components/homeComponents/stepIcons.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function StepIcons({icon,align}) {
   return (
     <div
-      className={`h-[100px]  relative w-[100px] max-sm:w-[70px] max-sm:h-[70px]   hover:scale-105 transition duration-100 ease-in ${align} `}
+      className={`h-[80px]  relative w-[80px] max-sm:w-[70px] max-sm:h-[70px]   hover:scale-105 transition duration-100 ease-in ${align} `}
     >
       <div className="absolute  border-2 border-white bg-primary w-full  h-full rounded-xl"></div>
       <div className=" text-[.9rem] w-full p-5 h-full bg-primary border-2 border-white  translate-x-2 -translate-y-2 flex items-center justify-center  rounded-xl font-medium">
--- src/components/homeComponents/steps.jsx
+++ src/components/homeComponents/steps.jsx
@@ -17,24 +17,37 @@ export default function Steps() {
       whileInView={{ width: "100%", borderRadius: "0px" }}
       initial={{ width: "80%", borderRadius: "100px" }}
       transition={{ duration: 1 }}
-      className="w-11/12  bg-primary h-full mx-auto p-20 max-sm:p-10"
+      className=" bg-primary h-full mx-auto p-20 max-sm:p-10"
     >
-      <div className="w-full flex justify-center max-sm:flex-col max-sm:gap-10">
+      <div className=" flex justify-center gap-0 max-sm:flex-col max-sm:gap-10">
         {/* Sub Hero Text  */}
-        <div>
-          <h1 className="text-4xl pb-10 font-semibold text-white w-1/2 max-sm:pb-5 max-sm:w-full">
-            A <span className='text-black-white-300'>Duo-Profit</span> ecosystem that creates partnership, not
-            tenancy
+        <div className="w-1/2 max-sm:w-full">
+          <h1 className="text-4xl pb-10 font-semibold text-white w-3/4 max-sm:text-2xl max-sm:pb-5 max-sm:w-full">
+            A <span className="text-black-white-300">Duo-Profit</span> ecosystem
+            that creates partnership, not tenancy
           </h1>
 
-          <ButtonSecondary text={"Start Investing"} width={"w-1/4 max-sm:w-full"}/>
+          <ButtonSecondary
+            text={"Start Investing"}
+            width={"w-1/4 max-sm:w-full"}
+          />
         </div>
 
         {/* Icons Avatar Wrapper Container */}
-        <div className='flex flex-col gap-10 w-1/3 max-sm:w-4/5 max-sm:items-center max-sm:mx-auto max-sm:gap-5'>
-          <StepIcons icon={group} align={'self-start'} />
-          <StepIcons icon={logo} align={'self-center'}  />
-          <StepIcons icon={home} align={'self-end'}/>
+        <div className="flex flex-col gap-10 w-1/4 relative max-sm:w-4/5 max-sm:pt-10 max-sm:items-center max-sm:mx-auto max-sm:gap-5">
+          <p className="absolute text-white bottom-4 left-0 text-[12px] w-1/2 -translate-x-10">
+            Lorem ipsum dolor sit, amet consectetur adipisicing.
+          </p>
+          <p className="absolute text-white top-4 right-4 text-[12px] w-1/2 ">
+            Lorem ipsum dolor sit, amet consectetur adipisicing.
+          </p>
+          <p className="absolute text-white top-1/2 right-0 text-[12px] w-1/2 translate-x-20 max-sm:translate-x-14 -translate-y-1/2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing.
+          </p>
+
+          <StepIcons icon={group} align={"self-start"} />
+          <StepIcons icon={logo} align={"self-center"} />
+          <StepIcons icon={home} align={"self-end"} />
         </div>
       </div>
     </motion.div>
