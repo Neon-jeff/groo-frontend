02d848e1adb08478a503f2ee220da8729b32e17b
Neon-jeff <omaksteejay@gmail.com>
2023-12-09 17:36:03 +0100

    FAQs and Stats

--- src/components/homeComponents/faqs.jsx
+++ src/components/homeComponents/faqs.jsx
@@ -5,7 +5,7 @@ export default function Faq() {
   return (
     <div className='flex px-10 mt-20 text-xl items-center justify-around w-4/5 mx-auto max-sm:flex-col max-sm:w-full max-sm:gap-10'>
       <div className=''>
-        <h1 className='text-5xl font-semibold pb-10'>FAQs</h1>
+        <h1 className='text-5xl font-semibold pb-10 max-sm:text-3xl'>FAQs</h1>
         <p className='w-1/2 max-sm:w-full'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus,
           tenetur ex. Voluptas omnis expedita nam animi laborum sequi
