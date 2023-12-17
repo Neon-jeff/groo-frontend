9525c8e585d09d6739d63d5c12f6c8507b84b5fc
Neon-jeff <omaksteejay@gmail.com>
2023-12-17 16:44:06 +0100

    Dashboard Aside Fix

--- src/components/dashboardComponents/AsideMobile.jsx
+++ src/components/dashboardComponents/AsideMobile.jsx
@@ -18,7 +18,7 @@ export default function AsideMobile({ show, setShow }) {
             ease: easeOut,
             duration: 0.3,
           }}
-          className="px-10 pt-20 fixed top-48 bg-black-white-100 rounded-t-3xl h-full w-full flex flex-col gap-10 items-center"
+          className="px-10 pt-20 fixed top-20 bottom-0 md:hidden bg-saltpan-100 rounded-t-3xl h-full w-full flex flex-col gap-5 items-center"
         >
           <IoCloseOutline
             size={30}
@@ -31,11 +31,11 @@ export default function AsideMobile({ show, setShow }) {
           <div className="w-full flex flex-col gap-5">
             <h1 className="text-xl font-semibold">Your Account Status</h1>
             <progress
-              className="progress progress-success w-56"
+              className="progress progress-success  w-56"
               value={20}
               max="100"
             ></progress>
-            <div className="bg-saltpan-100  rounded-lg p-5 text-sm flex flex-col gap-2">
+            <div className="bg-white  rounded-lg p-5 text-sm flex flex-col gap-2">
               <p className="text-lg font-semibold ">Verify your Account</p>
               <p>Steps</p>
               <p className="flex flex-col gap-1">
