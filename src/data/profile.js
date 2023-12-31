4f4e9c8e961a27e46c0bbdd2147d4efd914762bf
Neon-jeff <omaksteejay@gmail.com>
2023-12-31 18:32:12 +0100

    Fix profile update

--- src/data/profile.js
+++ src/data/profile.js
@@ -3,12 +3,9 @@ import { userStore } from "./store";
 import axios from "axios";
 import { csrfcookie } from "./token";
 import Cookies from "universal-cookie";
-import { useModal,useLoader } from "./store";
+import { useModal, useLoader } from "./store";
 
-
-let cookie=new Cookies()
-
-
+let cookie = new Cookies();
 
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
@@ -17,16 +14,19 @@ axios.defaults.headers = {
   Authorization: `Token ${JSON.parse(localStorage.getItem("auth"))}`,
 };
 
+// https://groobackend-production.up.railway.app
+// http://127.0.0.1:8000/
+
 const client = axios.create({
   baseURL: "https://groobackend-production.up.railway.app",
 });
 
-export default async function updateProfile(data){
+export default async function updateProfile(data) {
   useLoader.setState({ loader: true });
   client
     .patch("/api/profile/", data)
     .then(function (res) {
-      localStorage.setItem("user", JSON.stringify(res.data));
+      // localStorage.setItem("user", JSON.stringify(res.data));
     })
     .catch((e) => {
       useLoader.setState({ loader: false });
@@ -37,9 +37,6 @@ export default async function updateProfile(data){
         localStorage.setItem("user", JSON.stringify(res.data));
         userStore.setState({ user: res.data });
         useLoader.setState({ loader: false });
-
       })
     );
-
-};
-
+}
